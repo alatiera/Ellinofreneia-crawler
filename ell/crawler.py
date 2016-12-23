1021d03b8ed45e2fb45b073d7e4de67ffa58fd29
Jordan Petridis <alatiera@openmailbox.org>
2016-12-23 19:42:15 +0200

    refactoring and added tv url extracting

--- ell/crawler.py
+++ ell/crawler.py
@@ -3,22 +3,47 @@ import re
 import youtube_dl
 import ytdl
 
+
+def get_radio_show(url):
+    """ Extract the urls of the radio shows"""
+    html = urllib.request.urlopen(url).read()
+    shows = re.findall(b'meta\sproperty="og:url".*content="(.*)"', html)
+    for i in shows:
+        radioshows.append(i.decode())
+        # print(i.decode())
+
+
+def get_tv_show(url):
+    html = urllib.request.urlopen(url).read()
+    # print(html.decode())
+    # TODO extract episode title also
+    shows = re.findall(b'href="/(television/tv-shows/video/\S+")', html)
+    for i in shows:
+        tvshows.append(site + i.decode())
+        # print(i.decode())
+
+
 # TODO +tv link
+site = 'http://ellinofreneianet.gr/'
 radiourl = 'http://www.ellinofreneianet.gr/radio/radio-shows-2.html'
-radiohtml = urllib.request.urlopen(radiourl).read()
-# print(radiohtml)
-# print(type(radiohtml))
+tvurl = 'http://ellinofreneianet.gr/television/tv-shows.html'
+radioshows = []
+tvshows = []
 
-# look for content links
-shows = re.findall(b'meta\sproperty="og:url".*content="(.*)"', radiohtml)
-content = []
 
-for i in shows:
-    content.append(i.decode())
-    print(i.decode())
+def main():
+    get_radio_show(radiourl)
+    get_tv_show(tvurl)
 
-# print(content)
+    # TODO proper dl options listing
+    # TODO fix tvdl in ydl_opts
+    with youtube_dl.YoutubeDL(ytdl.ydl_opts) as ydl:
+        ydl.download([radioshows[0]])
 
-# TODO proper dl options listing
-with youtube_dl.YoutubeDL(ytdl.ydl_opts) as ydl:
-    ydl.download([content[0]])
+    # # testing
+    # for i in tvshows:
+    #     print(i)
+
+
+
+main()

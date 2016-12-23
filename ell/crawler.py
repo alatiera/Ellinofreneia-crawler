ee61c92ca26a8d8c634c8d74c63ef2d9acc9875a
Jordan Petridis <alatiera@openmailbox.org>
2016-12-23 16:40:05 +0200

    initial commit

---
+++ ell/crawler.py
@@ -0,0 +1,13 @@
+import urllib.request
+import re
+
+# TODO +tv link
+radiourl = 'http://www.ellinofreneianet.gr/radio/radio-shows-2.html'
+radiohtml = urllib.request.urlopen(radiourl).read()
+# print(radiohtml)
+# print(type(radiohtml))
+
+# look for content links
+shows = re.findall(b'meta\sproperty.*content="(.*)"', radiohtml)
+for show in shows:
+    print(show.decode())

c581c164a5b16a0b3be496aca003cbcbebc7e902
Jordan Petridis <alatiera@openmailbox.org>
2017-01-12 14:58:06 +0200

    minor changes

--- ell/renamer.py
+++ ell/renamer.py
@@ -19,31 +19,38 @@ months = {
         }
 
 
-def getDate(a):
-    try:
-        year = re.findall('\s([0-9]{4})', a)[0]
-        date = re.findall('\s(\d{1,2})\D{2}', a)[0]
-        month = a.split()[5]
-        if len(date) == 1:
-            date = '0' + date
-        # a at the end can be replaced with '.mp3')
-        os.rename(a, year + '-' + months[month] + '-' + date + ' ' + a)
-    except IndexError:
-        print('Non matching file')
+def getDate(fil):
+    """Extracts the date from file's title"""
+    year = re.findall('\s([0-9]{4})', fil)[0]
+    day = re.findall('\s(\d{1,2})\D{2}', fil)[0]
+    month = fil.split()[5]
+    if len(day) == 1:
+        day = '0' + day
+    date = year + '-' + months[month] + '-' + day
+    return date
+
+
+def renamer(date, fil):
+    # fil at the end can be replaced with '.mp3')
+    os.rename(fil, date + ' - ' + fil)
+    print('done!')
 
 
 def main():
-    c = os.listdir(path='.')
-    for i in c:
+    ls = os.listdir(path='.')
+    for i in ls:
         if re.search('^[0-9]+-[0-9]+-[0-9]+', i):
             # name reversing
             # can also be used in opposite to keep only the date
-            # os.rename(i, i[10:])
+            # os.rename(i, i[13:])
 
             print('alrdy good')
-            continue
         else:
-            getDate(i)
+            try:
+                renamer(getDate(i), i)
+            except IndexError:
+                print('Non matching file')
+                continue
 
 
 main()

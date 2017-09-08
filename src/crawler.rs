d0d0fd43eb80a17ef8f1f3447bf1396c78535b9b
Jordan Petridis <jordanpetridis@protonmail.com>
2017-09-08 21:55:33 +0300

    Implemented page_limit tha figures the content_limit of the page.

--- src/crawler.rs
+++ src/crawler.rs
@@ -11,18 +11,55 @@ pub fn main() -> Result<()> {
     let document = Document::from_read(res)?;
     let document1 = Document::from_read(res1)?;
 
+    let radio_links = get_radio_shows(&document);
+    let tv_links = get_tv_shows(&document1);
+
+    println!("{:#?}", radio_links);
+    println!("{:#?}", tv_links);
+
+    let limit = page_limit(&document);
+    let limit1 = page_limit(&document1);
+    println!("{:?}", limit);
+    println!("{:?}", limit1);
+
+    Ok(())
+}
+
+fn get_radio_shows(document: &Document) -> Vec<&str> {
+
     let radio_links: Vec<_> = document
         .find(Class("my-item"))
         .filter_map(|x| x.attr("href"))
         .collect();
 
-    let tv_links: Vec<_> = document1
+    radio_links
+}
+
+fn get_tv_shows(document: &Document) -> Vec<&str> {
+
+    let tv_links: Vec<_> = document
         .find(Class("yendif-thumbnail"))
         .filter_map(|x| x.attr("href"))
         .collect();
 
-    println!("{:#?}", radio_links);
-    println!("{:#?}", tv_links);
+    tv_links
+}
 
-    Ok(())
+fn page_limit(document: &Document) -> Option<i32> {
+    let limit = document.find(Attr("title", "Τέλος")).next();
+
+    // println!("{:?}", limit);
+    match limit {
+        Some(foo) => {
+            // It feels like regex
+            let stop = foo.attr("href").unwrap();
+            let mut bar: Vec<_> = stop.split("=").collect();
+            bar.reverse();
+            let num = bar.first().unwrap().parse().unwrap();
+
+            // println!("{:?}", num);
+            return Some(num);
+        } 
+        _ => None,
+    }
 }

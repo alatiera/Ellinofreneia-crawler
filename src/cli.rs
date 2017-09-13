0ff4f5f19bc5ae9e1b525ba669c244ef726c07a7
Jordan Petridis <jordanpetridis@protonmail.com>
2017-09-14 01:45:53 +0300

    Struct opt WIP.

--- src/cli.rs
+++ src/cli.rs
@@ -5,35 +5,48 @@ use errors::*;
 #[derive(StructOpt, Debug)]
 #[structopt(name = "example", about = "An example of StructOpt usage.")]
 struct Opt {
-    /// A flag, true if used in the command line.
-    // #[structopt(short = "d", long = "debug", help = "Activate debug mode")]
-    // debug: bool,
-
-    // ... Rustfmt hack
-    /// Number of Episodes to be Downloaded
-    #[structopt(short = "a", long = "amount", default_value = "10")]
-    amount: i64,
-
     /// Enable logging, use multiple `v`s to increase verbosity
     #[structopt(short = "v", long = "verbose")]
     verbosity: u64,
+
+    #[structopt(subcommand)]
+    dl: Opt1,
 }
 
+#[derive(StructOpt, Debug)]
+#[structopt(name = "Ellinofreneia-Crawler")]
+/// the stupid content tracker
+enum Opt1 {
+    #[structopt(name = "download")]
+    /// Download latest Episodes!
+    Download {
+        #[structopt(long = "tv")]
+        tv: bool,
+        #[structopt(long = "radio")]
+        radio: bool,
+
+        #[structopt(short = "a", long = "all", default_value = "true")]
+        all: bool,
+
+        /// Number of Episodes to be Downloaded
+        #[structopt(default_value = "10")]
+        amount: Option<i64>,
+    },
+}
 
 pub fn run() -> Result<()> {
     let args = Opt::from_args();
 
     loggerv::init_with_verbosity(args.verbosity)?;
 
-    // ...
-    // let thing = "foobar";
-    // debug!("Thing happened: {}", thing);
-    // ...
+    let foo = args.dl;
+    // This works
+    info!("{:?}", foo);
 
-    // info!("It's all good!");
+    // This dsnt and I hate everything
+    // info!("{:?}", foo.all);
 
-    info!("Speed: {:?}", args.amount);
-    ::crawler::latest_radio(args.amount as u64)?;
-    // ::crawler::latest_tv(args.amount as u64)?;
+    // ::crawler::latest_radio(args.amount as u64)?;
+    // ::crawler::latest_tv(am as u64)?;
     Ok(())
 }
